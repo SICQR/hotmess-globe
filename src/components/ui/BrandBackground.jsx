f881eeac4fae8533e6527e08af1c015b14aede21
SICQR <151458843+SICQR@users.noreply.github.com>
2026-03-01 21:04:57 +0700

    Change background color in BrandBackground component

--- src/components/ui/BrandBackground.jsx
+++ src/components/ui/BrandBackground.jsx
@@ -12,7 +12,7 @@ export default function BrandBackground() {
       />
       <div
         className="absolute -bottom-32 -right-32 w-[400px] h-[400px] rounded-full blur-3xl opacity-20 animate-pulse"
-        style={{ background: '#B026FF', animationDelay: '1.2s' }}
+        style={{ background: '#C8962C', animationDelay: '1.2s' }}
       />
       <div
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-64 h-64 rounded-full blur-3xl opacity-10 animate-pulse"
